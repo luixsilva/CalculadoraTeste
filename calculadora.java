60610ff16aabbac243a25d35119ce20f2066759d
luixsilva <felipeluiz75@outlook.com>
2023-09-21 12:19:45 -0300

    Alterações

---
+++ calculadora.java
@@ -0,0 +1,15 @@
+public class calculadora{
+    public int somar(int a, int b){
+     return a + b;   
+    }
+
+    public int subtraçao (int a , int b){
+    return  a - b;
+}
+    public int divisao (int a , int b){
+     return  a / b;
+}
+    public int multiplicaçao(int a , int b){
+        return  a * b;
+     }
+}
